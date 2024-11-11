0c67e27d10d4a1604ab77d87a42f053aa6b27cb4
Aldwin Rich R. Villanueva <Aldwinrich90@gmail.com>
2024-11-11 16:35:38 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-
+  <div class="bg-grey w-screen h-screen">
+    <H1>{{  title }}</H1>
   </div>
 </template>
 
 <script>
   export default {
+    components: [],
     data() {
       return {
-
+        title: 'try',
       }
     },
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-
-
+import './assets/styles.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Valorant_Api from './assets/Valorant_Api.vue'
 
+App.components('Valorantapi',Valorant_Api)
 createApp(App).mount('#app')
